1bb5c7b2d2d34641a430e9da2adf74db6e491821
leon <github@qaz.dds.nl>
2024-12-03 12:22:30 +0100

    Added baggage and span attributes

--- api-service/src/main/java/com/example/demo/Controller.java
+++ api-service/src/main/java/com/example/demo/Controller.java
@@ -1,11 +1,21 @@
 package com.example.demo;
 
+import io.micrometer.observation.annotation.Observed;
+import io.micrometer.tracing.BaggageInScope;
+import io.micrometer.tracing.Tracer;
+import io.micrometer.tracing.annotation.NewSpan;
+import io.micrometer.tracing.annotation.SpanTag;
+import io.opentelemetry.api.baggage.Baggage;
+import io.opentelemetry.api.baggage.BaggageEntry;
+import io.opentelemetry.context.Context;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.function.Supplier;
 
 @RestController
 public class Controller {
@@ -14,20 +24,52 @@ public class Controller {
 
   private AddressClient addressClient;
 
+  private final Tracer tracer;
+
   private Logger logger = LoggerFactory.getLogger(Controller.class);
 
   @Autowired
-  public Controller(CustomerClient customerClient, AddressClient addressClient) {
+  public Controller(CustomerClient customerClient, AddressClient addressClient, Tracer tracer) {
     this.customerClient = customerClient;
     this.addressClient = addressClient;
+    this.tracer = tracer;
+  }
+
+  @NewSpan("newspan")
+  @GetMapping(path = "oldcustomers/{id}")
+  public CustomerAndAddress getOldCustomerWithAddress(@SpanTag("spantag") @PathVariable("id") long customerId){
+    var usecase = "old-customers";
+    addUsecaseToSpan(usecase);
+    return withUseCaseBaggage(usecase, CustomerAndAddress.class, () -> {
+      logger.info("COLLECTING OLD CUSTOMER AND ADDRESS WITH ID {} FROM UPSTREAM SERVICE", customerId);
+      Customer customer = customerClient.getCustomer(customerId);
+      Address address = addressClient.getAddressForCustomerId(customerId);
+      return new CustomerAndAddress(customer, address);
+    });
   }
 
   @GetMapping(path = "customers/{id}")
   public CustomerAndAddress getCustomerWithAddress(@PathVariable("id") long customerId){
-    logger.info("COLLECTING CUSTOMER AND ADDRESS WITH ID {} FROM UPSTREAM SERVICE", customerId);
-    Customer customer = customerClient.getCustomer(customerId);
-    Address address = addressClient.getAddressForCustomerId(customerId);
-    return new CustomerAndAddress(customer, address);
+    var usecase = "customers";
+    addUsecaseToSpan(usecase);
+    return withUseCaseBaggage(usecase, CustomerAndAddress.class, () -> {
+      logger.info("COLLECTING CUSTOMER AND ADDRESS WITH ID {} FROM UPSTREAM SERVICE", customerId);
+      Customer customer = customerClient.getCustomer(customerId);
+      Address address = addressClient.getAddressForCustomerId(customerId);
+      return new CustomerAndAddress(customer, address);
+    });
   }
 
+  private void addUsecaseToSpan(String usecase) {
+    var span = tracer.currentSpan();
+    if (span != null) {
+      span.tag("use-case-tag", usecase);
+    }
+  }
+
+  private <T> T withUseCaseBaggage(String usecase, Class<T> clazz, Supplier<T>supplier) {
+    try (var ignored = this.tracer.createBaggageInScope("use-case-baggage", usecase)) {
+      return supplier.get();
+    }
+  }
 }
--- customer-service/src/main/java/com/example/demo/Controller.java
+++ customer-service/src/main/java/com/example/demo/Controller.java
@@ -3,7 +3,12 @@ package com.example.demo;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
 
+import io.micrometer.tracing.Tracer;
+import io.opentelemetry.api.baggage.Baggage;
+import io.opentelemetry.api.baggage.BaggageEntry;
+import io.opentelemetry.context.Context;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
@@ -15,137 +20,146 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 
-  private Logger logger = LoggerFactory.getLogger(Controller.class);
+    private Logger logger = LoggerFactory.getLogger(Controller.class);
 
-  @GetMapping(path = "customers/{id}")
-  public ResponseEntity<Customer> getCustomer(@PathVariable("id") long customerId) {
-    logger.info("GETTING CUSTOMER WITH ID {}", customerId);
-    if (customerId < 0 || customerId > NAMES.size() - 1) {
-      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    @GetMapping(path = "customers/{id}")
+    public ResponseEntity<Customer> getCustomer(@PathVariable("id") long customerId) {
+        logger.info("GETTING CUSTOMER WITH ID {}", customerId);
+        logBaggage();
+        if (customerId < 0 || customerId > NAMES.size() - 1) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        Customer customer = new Customer();
+        customer.setId(customerId);
+        customer.setName(NAMES.get((int) customerId));
+
+        return new ResponseEntity<>(customer, HttpStatus.OK);
     }
-    Customer customer = new Customer();
-    customer.setId(customerId);
-    customer.setName(NAMES.get((int) customerId));
 
-    return new ResponseEntity<>(customer, HttpStatus.OK);
-  }
-
-  @GetMapping(path = "addresses/{id}")
-  public ResponseEntity<Address> getAddress(@PathVariable("id") long customerId) throws InterruptedException {
-    logger.info("GETTING ADDRESS FOR CUSTOMER WITH ID {}", customerId);
-    if (customerId < 0 || customerId > NAMES.size() - 1) {
-      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    private void logBaggage() {
+        io.opentelemetry.api.baggage.Baggage baggage = Baggage.fromContext(Context.current()); // opentelemtry API. TODO check how micrometer does tgis
+        baggage.asMap()
+                .entrySet()
+                .forEach(entry -> logger.info("Baggage [{},{}]", entry.getKey(), entry.getValue()));
     }
-    Address address = new Address();
-    address.setId(customerId);
-    address.setStreet(STREETS.get((int) customerId));
 
-    return new ResponseEntity<>(address, HttpStatus.OK);
-  }
+    @GetMapping(path = "addresses/{id}")
+    public ResponseEntity<Address> getAddress(@PathVariable("id") long customerId) {
+        logBaggage();
+        logger.info("GETTING ADDRESS FOR CUSTOMER WITH ID {}", customerId);
+        if (customerId < 0 || customerId > NAMES.size() - 1) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        Address address = new Address();
+        address.setId(customerId);
+        address.setStreet(STREETS.get((int) customerId));
 
-  private static final List<String> STREETS = Arrays.asList(
-          "Jefferson Street",
-          "Cambridge Road",
-          "Olive Street",
-          "Old York Road",
-          "Aspen Drive",
-          "Cherry Lane",
-          "10th Street",
-          "Pennsylvania Avenue",
-          "Central Avenue",
-          "8th Street West",
-          "Route 11",
-          "Street Road",
-          "Prospect Street",
-          "Devon Road",
-          "Forest Avenue",
-          "Elm Avenue",
-          "Heather Court",
-          "Route 202",
-          "Willow Drive",
-          "Evergreen Lane",
-          "Windsor Drive",
-          "Mulberry Street",
-          "2nd Street North",
-          "Sheffield Drive",
-          "Ashley Court",
-          "Valley Road",
-          "Spruce Street",
-          "Wood Street",
-          "Bridge Street",
-          "Monroe Drive",
-          "Church Road",
-          "Main Street South",
-          "Adams Avenue",
-          "Delaware Avenue",
-          "Somerset Drive",
-          "9th Street",
-          "3rd Street East",
-          "Augusta Drive",
-          "Wall Street",
-          "Broad Street",
-          "Route 2",
-          "Fairview Avenue",
-          "5th Avenue",
-          "Spruce Avenue",
-          "Cemetery Road",
-          "River Street",
-          "Tanglewood Drive",
-          "Cedar Street",
-          "Canal Street",
-          "Market Street");
+        return new ResponseEntity<>(address, HttpStatus.OK);
+    }
 
-  private static final List<String> NAMES = Arrays.asList(
-          "Daria Domino",
-          "Yukiko Yawn",
-          "Diane Dalessio",
-          "Elijah Elmore",
-          "Tyron Thaler",
-          "Stefani Stayton",
-          "Shara Stam",
-          "Earlean Eblin",
-          "Jonell Janecek",
-          "Thaddeus Tupper",
-          "Dorthy Delany",
-          "Shanel Saffell",
-          "Ricki Rost",
-          "Kandy Knecht",
-          "Mirella Ma",
-          "Rocky Ros",
-          "Theo Tosi",
-          "Marnie Manzer",
-          "Talisha Trottier",
-          "Thanh Tamez",
-          "Hyacinth Hotard",
-          "Dianne Dearman",
-          "Marc Medlin",
-          "Roxane Rossin",
-          "Mellie Marino",
-          "Eugene Eng",
-          "Charis Curnutt",
-          "Lillia Leming",
-          "Gretta Gately",
-          "Jessica Johnsen",
-          "Raven Riera",
-          "Roni Rost",
-          "Garth Gipson",
-          "Demetrice Dell",
-          "Eugenie Ead",
-          "Jared Julio",
-          "Alaina Aguinald ",
-          "Hortense Haun",
-          "Willia Woodland",
-          "Jessenia Jefferis",
-          "Jinny June",
-          "William Whang",
-          "Dessie Down",
-          "Katelyn Koren",
-          "Barbara Bazaldua",
-          "France Frese",
-          "Eufemia Evangelista",
-          "Neva Nock",
-          "Rickey Redd",
-          "Norma Nagler");
+    private static final List<String> STREETS = Arrays.asList(
+            "Jefferson Street",
+            "Cambridge Road",
+            "Olive Street",
+            "Old York Road",
+            "Aspen Drive",
+            "Cherry Lane",
+            "10th Street",
+            "Pennsylvania Avenue",
+            "Central Avenue",
+            "8th Street West",
+            "Route 11",
+            "Street Road",
+            "Prospect Street",
+            "Devon Road",
+            "Forest Avenue",
+            "Elm Avenue",
+            "Heather Court",
+            "Route 202",
+            "Willow Drive",
+            "Evergreen Lane",
+            "Windsor Drive",
+            "Mulberry Street",
+            "2nd Street North",
+            "Sheffield Drive",
+            "Ashley Court",
+            "Valley Road",
+            "Spruce Street",
+            "Wood Street",
+            "Bridge Street",
+            "Monroe Drive",
+            "Church Road",
+            "Main Street South",
+            "Adams Avenue",
+            "Delaware Avenue",
+            "Somerset Drive",
+            "9th Street",
+            "3rd Street East",
+            "Augusta Drive",
+            "Wall Street",
+            "Broad Street",
+            "Route 2",
+            "Fairview Avenue",
+            "5th Avenue",
+            "Spruce Avenue",
+            "Cemetery Road",
+            "River Street",
+            "Tanglewood Drive",
+            "Cedar Street",
+            "Canal Street",
+            "Market Street");
+
+    private static final List<String> NAMES = Arrays.asList(
+            "Daria Domino",
+            "Yukiko Yawn",
+            "Diane Dalessio",
+            "Elijah Elmore",
+            "Tyron Thaler",
+            "Stefani Stayton",
+            "Shara Stam",
+            "Earlean Eblin",
+            "Jonell Janecek",
+            "Thaddeus Tupper",
+            "Dorthy Delany",
+            "Shanel Saffell",
+            "Ricki Rost",
+            "Kandy Knecht",
+            "Mirella Ma",
+            "Rocky Ros",
+            "Theo Tosi",
+            "Marnie Manzer",
+            "Talisha Trottier",
+            "Thanh Tamez",
+            "Hyacinth Hotard",
+            "Dianne Dearman",
+            "Marc Medlin",
+            "Roxane Rossin",
+            "Mellie Marino",
+            "Eugene Eng",
+            "Charis Curnutt",
+            "Lillia Leming",
+            "Gretta Gately",
+            "Jessica Johnsen",
+            "Raven Riera",
+            "Roni Rost",
+            "Garth Gipson",
+            "Demetrice Dell",
+            "Eugenie Ead",
+            "Jared Julio",
+            "Alaina Aguinald ",
+            "Hortense Haun",
+            "Willia Woodland",
+            "Jessenia Jefferis",
+            "Jinny June",
+            "William Whang",
+            "Dessie Down",
+            "Katelyn Koren",
+            "Barbara Bazaldua",
+            "France Frese",
+            "Eufemia Evangelista",
+            "Neva Nock",
+            "Rickey Redd",
+            "Norma Nagler");
 
 
 }
